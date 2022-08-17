252cf7961c104f458e92ebd789d538b084c166b7
superkaplis <dzintarsmartins@gmail.com>
2022-08-17 21:40:28 +0300

    added button functionality

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const container = document.getElementById('container')
 
-for(i = 0; i < 256; i++){
-  const div = document.createElement('div');
-  div.classList.add('griddy')
+
+function populateGrid(sqrCount, sqrWidth) {
   
-  div.addEventListener('mouseover', () => {
-    div.style = "background-color: black;"
+  const oldSquares = document.querySelectorAll('.griddy')
+
+  oldSquares.forEach(element => {
+    element.remove();
   })
-  
-  container.appendChild(div);
+
+
+  for (i = 0; i < sqrCount; i++) {
+    const div = document.createElement('div');
+    div.classList.add('griddy')
+    div.style.width = `${sqrWidth}%`
+    div.style.height = `${sqrWidth}%`
+
+
+
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'black'
+    })
+
+    container.appendChild(div);
+  }
+
 }
+
+populateGrid(256);
+
+const newGridBtn = document.getElementById('newGridBtn');
+
+newGridBtn.addEventListener('click', function() {
+  let sqrNum = prompt('Enter squares per side:')
+  let returnVal = sqrNum * sqrNum;
+  let widthHeight = (100 / sqrNum);
+  populateGrid(returnVal, widthHeight);
+})
